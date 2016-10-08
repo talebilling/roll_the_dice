6979af22f53a58845fa6adc3241ef1242b88d01b
Emese Billing <talebilling@gmail.com>
2016-10-08 16:37:52 +0200

    slit up into functions

--- roll_the_dice.py
+++ roll_the_dice.py
@@ -1,6 +1,28 @@
 import random
 
-mylist
-random.randrange[1:7]
+def append_random(player):
+    player.append(random.randrange(1, 7))
 
-print(mylist)
+def sorting_dice(player):
+    player.sort(reverse=True)
+    
+def play(player_1, player_2):
+    for i in range(3):
+        append_random(player_1)
+    for i in range(2):
+        append_random(player_2)
+
+    sorting_dice(attacker)
+    sorting_dice(defender)
+
+def print_result(player_1, player_2):
+    print('Dice:')
+    print("{}{}{}{}{}" .format(player_1[0],'-', player_1[1], '-', player_1[2]))
+    print("{}{}{}" .format(player_2[0],'-', player_2[1]))
+
+attacker = []
+defender = []
+
+play(attacker, defender)
+
+print_result(attacker, defender)
